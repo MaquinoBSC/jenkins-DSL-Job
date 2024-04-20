f81af0fdd4108461c0cb0ace2c51703e8bf1b2ce
MaquinoBSC <martinare.ipn@gmail.com>
2024-04-19 21:10:36 -0600

    add jobParametrizado.groovy file

---
+++ jobParametrizado.groovy
@@ -0,0 +1,40 @@
+job('ejemplo-job-DSL') {
+  description('Job DSL de ejemplo para el curso de Jenkins y creado por el maquino')
+  scm {
+    git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node -> // is hudson.plugins.git.GitSCM
+      node / gitConfigName('MaquinoBSC')
+      node / gitConfigEmail('martinare.ipn@gmail.com')
+    }
+  }
+  parameters{
+    stringParam('nombre', defaultValue = 'Martin', description = 'Parametro de cadena para el Job Booleano')
+    choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+    booleanParam('agente', false)
+  }
+  triggers{
+    cron('H/7 * * * *')
+  }
+  steps {
+    shell("bash jobscript.sh")
+  }
+  publishers {
+    mailer('martinare.ipn@gmail.com', true, true)
+    slackNotifier {
+      notifyAborted(true)
+      notifyEveryFailure(true)
+      notifyNotBuilt(false)
+      notifyUnstable(false)
+      notifyBackToNormal(true)
+      notifySuccess(true)
+      notifyRepeatedFailure(false)
+      startNotification(false)
+      includeTestSummary(false)
+      includeCustomMessage(false)
+      customMessage(null)
+      sendAs(null)
+      commitInfoChoice('NONE')
+      teamDomain(null)
+      authToken(null)
+    }
+  }
+}
